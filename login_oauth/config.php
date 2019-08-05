10d0d499445137f56a71f953d10ddf97d3d1837e
francisco <chikocorrales@hotmail.com>
2019-08-04 20:25:48 -0600

    login App en php con google dev

---
+++ login_oauth/config.php
@@ -0,0 +1,9 @@
+<?php
+/* App Client Id */
+define('APP_CLIENT_ID', '847332093695-467fpdnoqaft7qvrnomlk6kupjcvegl9.apps.googleusercontent.com');
+/* App Client Secret */
+define('APP_CLIENT_SECRET', 'Y3TfZzx2Kl1AFJ-M7_Hg1W1f');
+/* App Redirect Url */
+define('APP_CLIENT_REDIRECT_URL', 'http://localhost:8080/login_oauth/login_oauth/result.php');
+
+?>
